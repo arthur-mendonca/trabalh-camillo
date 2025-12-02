427339f0130240b0f71627756b351a5c6c0412ec
PHAlmeida <pedro.almeida@viannasempre.com.br>
2025-12-01 21:13:57 -0300

    fix(main): habilitar CORS para múltiplas portas

--- library-backend/src/main.ts
+++ library-backend/src/main.ts
@@ -7,6 +7,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: ['http://localhost:3001', 'http://localhost:3002'],
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe(
     { whitelist: true, forbidNonWhitelisted: true, transform: true }
   ));
